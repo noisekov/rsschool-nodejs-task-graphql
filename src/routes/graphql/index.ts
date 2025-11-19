540e678aebee0aec4d9a9849680f0c216faa489f
noisekov <noise123q@mail.ru>
2025-11-19 22:03:19 +0300

    feat: one test pass

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,22 +1,68 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import {
+  graphql,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+import type { MemberType } from '@prisma/client';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
 
-  fastify.route({
-    url: '/',
-    method: 'POST',
-    schema: {
-      ...createGqlResponseSchema,
-      response: {
-        200: gqlResponseSchema,
+  const MemberType = new GraphQLObjectType({
+    name: 'MemberType',
+    fields: {
+      id: { type: GraphQLString },
+      discount: { type: GraphQLFloat },
+      postsLimitPerMonth: { type: GraphQLInt },
+    },
+  });
+
+  const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+      name: 'Query',
+      fields: {
+        memberTypes: {
+          type: new GraphQLList(MemberType),
+          resolve: async (): Promise<MemberType[]> => await prisma.memberType.findMany(),
+        },
       },
+    }),
+  });
+
+  fastify.route({
+    url: '/member-types',
+    method: 'GET',
+    async handler() {
+      const result = await graphql({
+        schema,
+        source: `query { memberTypes { id discount postsLimitPerMonth } }`,
+        contextValue: { prisma },
+      });
+
+      return result.data?.memberTypes || [];
     },
-    async handler(req) {
-      // return graphql();
-    },
+    //   schema: {
+    //     ...createGqlResponseSchema,
+    //     response: {
+    //       200: gqlResponseSchema,
+    //     },
+    //   },
+    //   async handler(req) {
+    //     const { query, variables } = req.body;
+
+    //     return await graphql({
+    //       schema,
+    //       source: query,
+    //       variableValues: variables,
+    //       contextValue: { prisma },
+    //     });
+    //   },
   });
 };
 
