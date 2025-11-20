e2e9bcae83c53189165af32b2046e4706004ed3f
noisekov <noise123q@mail.ru>
2025-11-20 21:31:54 +0300

    feat: almost worked script

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import {
   graphql,
+  GraphQLBoolean,
   GraphQLFloat,
   GraphQLInt,
   GraphQLList,
@@ -9,12 +10,13 @@ import {
   GraphQLSchema,
   GraphQLString,
 } from 'graphql';
-import type { MemberType } from '@prisma/client';
+import { UUIDType } from './types/uuid.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
 
-  const MemberType = new GraphQLObjectType({
+  // MemberType
+  const MemberTypeGQL = new GraphQLObjectType({
     name: 'MemberType',
     fields: {
       id: { type: GraphQLString },
@@ -23,16 +25,63 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     },
   });
 
-  const schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-      name: 'MemberTypes',
-      fields: {
-        memberTypes: {
-          type: new GraphQLList(MemberType),
-          resolve: async (): Promise<MemberType[]> => await prisma.memberType.findMany(),
-        },
+  // Post
+  const PostGQL = new GraphQLObjectType({
+    name: 'Post',
+    fields: {
+      id: { type: UUIDType },
+      title: { type: GraphQLString },
+      content: { type: GraphQLString },
+      authorId: { type: UUIDType },
+    },
+  });
+
+  // Profile
+  const ProfileGQL = new GraphQLObjectType({
+    name: 'Profile',
+    fields: {
+      id: { type: UUIDType },
+      isMale: { type: GraphQLBoolean },
+      yearOfBirth: { type: GraphQLInt },
+      memberTypeId: { type: GraphQLString },
+      userId: { type: UUIDType },
+    },
+  });
+
+  // User
+  const UserGQL = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+      id: { type: UUIDType },
+      name: { type: GraphQLString },
+      balance: { type: GraphQLFloat },
+    },
+  });
+
+  const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      memberTypes: {
+        type: new GraphQLList(MemberTypeGQL),
+        resolve: async () => await prisma.memberType.findMany(),
       },
-    }),
+      posts: {
+        type: new GraphQLList(PostGQL),
+        resolve: async () => await prisma.post.findMany(),
+      },
+      profiles: {
+        type: new GraphQLList(ProfileGQL),
+        resolve: async () => await prisma.profile.findMany(),
+      },
+      users: {
+        type: new GraphQLList(UserGQL),
+        resolve: async () => await prisma.user.findMany(),
+      },
+    },
+  });
+
+  const schema = new GraphQLSchema({
+    query: RootQuery,
   });
 
   fastify.route({
@@ -47,7 +96,7 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       return graphql({
         schema,
-        source: `query { memberTypes { id discount postsLimitPerMonth } }`,
+        source: req.body.query,
         variableValues: req.body.variables,
         contextValue: { prisma },
       });
